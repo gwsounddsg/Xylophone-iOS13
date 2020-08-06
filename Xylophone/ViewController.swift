dde73f424d0d798f1c808107d47fbbe9a3155e83
GW <gwsounddsg@gmail.com>
2020-08-06 13:11:28 -0700

    connects buttons to ViewController, keyPressed plays a sound associated with the title of the button

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+
+    var player: AVAudioPlayer!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        guard let title = sender.currentTitle else {return}
+        playSound(title)
+    }
     
 
+    func playSound(_ note: String) {
+        guard let url = Bundle.main.url(forResource: note, withExtension: "wav") else {
+            return
+        }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.play()
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+    }
 }
 
